fca19ffa9a35778441fa9a2b5c2f98eeacd01f42
Ryan Poplin <mobileupstate@gmail.com>
2013-10-10 01:39:24 -0400

    Additions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+// jQuery(document).ready(function(){
+console.log('jQuery ' + jQuery.fn.jquery);
+// alert(jQuery('p').text());
+// console.log('Page contains ' + jQuery('a').length + ' <a></a> element/s...');
+// console.log(jQuery('a, p').length);
+// console.log(jQuery(document.getElementsByTagName('a')).length);
+// jQuery utilizes 'Sizzle'.
+// });
